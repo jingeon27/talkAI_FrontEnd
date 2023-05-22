e4284f08b5fa2f846a1c6106c141b464938cebdf
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-05-22 14:32:38 +0900

    feat : styled-components setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import Image from "next/image";
-
+import styled from "styled-components";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <_Div>안녕하세요.</_Div>
+    </main>
+  );
 }
+const _Div = styled.div``;
